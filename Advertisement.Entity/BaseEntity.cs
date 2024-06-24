1957ddaaa49a6668be3296e6004a9791615d1d04
Ozan Ulus <ilkerozanulus@gmail.com>
2024-06-25 00:03:50 +0300

    created advertisementcontext

--- Advertisement.Entity/BaseEntity.cs
+++ Advertisement.Entity/BaseEntity.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Advertisement.Entity
+namespace AdvertisementApp.Entity
 {
     public class BaseEntity
     {
--- AdvertisementCommon/CustomValidationError.cs
+++ AdvertisementCommon/CustomValidationError.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Advertisement.Common
+namespace AdvertisementApp.Common
 {
     public class CustomValidationError
     {
