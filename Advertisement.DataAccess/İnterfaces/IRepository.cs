26a12876f3794e2565bdf86df19aa041ca0bfcfc
Ozan Ulus <ilkerozanulus@gmail.com>
2024-06-25 01:05:37 +0300

    created repository

---
+++ Advertisement.DataAccess/İnterfaces/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using AdvertisementApp.Common.Enums;
+using AdvertisementApp.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvertisementApp.DataAccess.İnterfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<List<T>> GetAllAsync();
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>> filter);
+        Task<List<T>> GetAllAsync<TKey>(Expression<Func<T, TKey>> selector, OrderByType orderByType =
+            OrderByType.DESC);
+        Task<List<T>> GetAllAsync<TKey>(Expression<Func<T, bool>> filter, Expression<Func<T, TKey>> selector,
+           OrderByType orderByType = OrderByType.DESC);
+        Task<T> FindAsync(object id);
+        Task<T> GetByFilterAsync(Expression<Func<T, bool>> filter, bool asNoTracking = false);
+        IQueryable<T> GetQueryable();
+        void Remove(T entity);
+        Task CreateAsync(T entity);
+        void Update(T entity, T unchanged);
+    }
+}
