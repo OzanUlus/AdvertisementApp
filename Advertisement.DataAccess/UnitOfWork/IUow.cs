3f76adb01ef52a909af5214906bb9c4be346bea7
Ozan Ulus <ilkerozanulus@gmail.com>
2024-06-25 01:16:35 +0300

    added uow

---
+++ Advertisement.DataAccess/UnitOfWork/IUow.cs
@@ -0,0 +1,16 @@
+﻿using AdvertisementApp.DataAccess.İnterfaces;
+using AdvertisementApp.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvertisementApp.DataAccess.UnitOfWork
+{
+    public interface IUow
+    {
+        IRepository<T> GetRepository<T>() where T : BaseEntity;
+        Task SaveChangesAsync();
+    }
+}
