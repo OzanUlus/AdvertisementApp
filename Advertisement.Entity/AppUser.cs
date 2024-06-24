21f1b1914b15c8ec6edd12dfcca447b15bcc0bef
Ozan Ulus <ilkerozanulus@gmail.com>
2024-06-24 23:36:13 +0300

    created entities

---
+++ Advertisement.Entity/AppUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertisement.Entity
+{
+    public class AppUser : BaseEntity
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public IEnumerable<AppUserRole> AppUserRoles { get; set; }
+        public int GenderId { get; set; }
+        public Gender Gender { get; set; }
+
+        public IEnumerable<AdvertisementAppUser> AdvertisementAppUsers { get; set; }
+    }
+}
---
+++ Advertisement.Entity/Gender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertisement.Entity
+{
+    public class Gender : BaseEntity
+    {
+        public string Definition { get; set; }
+        public IEnumerable<AppUser> AppUsers { get; set; }
+    }
+}
