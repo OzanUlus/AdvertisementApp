e73b3e6e23f42a6a0f11bffa0a622886023697b7
Ozan Ulus <ilkerozanulus@gmail.com>
2024-06-25 16:52:16 +0300

    providedservice mapping

---
+++ AdvertisementApp.Business/Mapping/ProvidedServiceProfile.cs
@@ -0,0 +1,21 @@
+﻿using AdvertisementApp.Dtos.ProvidedService;
+using AdvertisementApp.Entity;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvertisementApp.Business.Mapping
+{
+    public class ProvidedServiceProfile : Profile
+    {
+        public ProvidedServiceProfile()
+        {
+            CreateMap<ProvidedService , ProvidedServiceCreateDto> ().ReverseMap();
+            CreateMap<ProvidedService , ProvidedServiceUpdateDto> ().ReverseMap();
+            CreateMap<ProvidedService , ProvidedServiceListDto> ().ReverseMap();
+        }
+    }
+}
