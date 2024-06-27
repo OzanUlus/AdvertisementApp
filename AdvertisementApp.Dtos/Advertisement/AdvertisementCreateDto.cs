bca340a616542aeb1a75588ef1f29babdfe6b886
Ozan Ulus <ilkerozanulus@gmail.com>
2024-06-28 00:20:55 +0300

    fixed

--- AdvertisementApp.Dtos/Advertisement/AdvertisementCreateDto.cs
+++ AdvertisementApp.Dtos/Advertisement/AdvertisementCreateDto.cs
@@ -12,6 +12,6 @@ namespace AdvertisementApp.Dtos
         public string Title { get; set; }
         public bool Status { get; set; }
         public string Description { get; set; }
-        public DateTime CreatedDate { get; set; }
+        
     }
 }
