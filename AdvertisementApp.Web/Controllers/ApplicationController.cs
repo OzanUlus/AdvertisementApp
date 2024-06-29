fa6b7291de7d6dae434ab4389807c1c85af0f03d
Ozan Ulus <ilkerozanulus@gmail.com>
2024-06-29 17:10:49 +0300

    advertisement create,update and delete

--- AdvertisementApp.Web/Controllers/ApplicationController.cs
+++ AdvertisementApp.Web/Controllers/ApplicationController.cs
@@ -1,4 +1,6 @@
 ﻿using AdvertisementApp.Business.Services;
+using AdvertisementApp.Dtos;
+using AdvertisementApp.Entity;
 using AdvertisementApp.Web.Extension;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -20,5 +22,42 @@ namespace AdvertisementApp.Web.Controllers
             var response = await _advertisementService.GetAllAsync();
             return this.ResponseView(response);
         }
+
+        public  IActionResult Create()
+        {
+            
+            return View(new AdvertisementCreateDto() );
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(AdvertisementCreateDto advertisementCreateDto)
+        {
+            await _advertisementService.CreateAsync(advertisementCreateDto);
+
+            return RedirectToAction("List");
+        }
+
+        public async Task<IActionResult> Delete(int advertisementId)
+        {
+           var response = await _advertisementService.RemoveAsync(advertisementId);
+
+            return this.ResponseRedirectAction(response,"List");
+        }
+
+        public async Task<IActionResult> Update(int advertisementId)
+        {
+            var response = await _advertisementService.GetByIdAsync<AdvertisementUpdateDto>(advertisementId);
+            return this.ResponseView(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(AdvertisementUpdateDto advertisementUpdateDto)
+        {
+          var response = await _advertisementService.UpdateAsync(advertisementUpdateDto);
+            return this.ResponseRedirectAction(response, "List");
+        }
+
+
+
     }
 }
