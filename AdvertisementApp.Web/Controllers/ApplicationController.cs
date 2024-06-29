ce4322605028ef26112a6d20f25c1eeb4d6bb90e
Ozan Ulus <ilkerozanulus@gmail.com>
2024-06-29 16:26:16 +0300

    advertisementlist

---
+++ AdvertisementApp.Web/Controllers/ApplicationController.cs
@@ -0,0 +1,24 @@
+﻿using AdvertisementApp.Business.Services;
+using AdvertisementApp.Web.Extension;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdvertisementApp.Web.Controllers
+{
+    [Authorize(Roles ="Admin")]
+    public class ApplicationController : Controller
+    {
+        private readonly IAdvertisementService _advertisementService;
+
+        public ApplicationController(IAdvertisementService advertisementService)
+        {
+            _advertisementService = advertisementService;
+        }
+
+        public async Task<IActionResult> List()
+        {
+            var response = await _advertisementService.GetAllAsync();
+            return this.ResponseView(response);
+        }
+    }
+}
